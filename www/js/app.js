9c8d484bc1411edd3068254711be6f6ed18054a4
anand <anand.pathak@innoraft.com>
2016-04-25 21:01:02 +0530

    improved UI and added font
    added font, improved setting page UI

--- www/js/app.js
+++ www/js/app.js
@@ -41,11 +41,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.contacts', {
+      url: '/contacts',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/contacts.html'
         }
       }
     })
